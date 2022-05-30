70dfb89155eb721118e8be052e1935b889fb1bab
Philipp Scheede <p.scheede@gmx.de>
2022-05-30 12:15:24 +0200

    Improve home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,33 @@
 <template>
-<h1>Philipp Scheede</h1>
-  <h2><a href="#/gallery">Photography</a></h2>
+  <div class="container">
+    <h1>Hi, my name is Philipp Scheede!</h1>
+    <h2>Have a look at my <a href="#/gallery">Photography</a>!</h2>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped lang="scss">
+<style scoped>
+.container {
+  padding-top: 20rem;
+  color: var(--text-color);
+}
 
+h1 {
+  margin-bottom: 2rem;
+}
+
+a {
+  color: var(--text-color);
+}
+
+a:visited {
+  color: var(--text-color);
+}
+
+a:hover {
+  color: var(--text-color-highlight);
+}
 </style>
