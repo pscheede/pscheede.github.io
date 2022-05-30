fadc989275d283ba7cfa953c63cef951e0e97174
Philipp Scheede <p.scheede@gmx.de>
2022-05-31 00:02:45 +0200

    Add localization

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createI18n} from "vue-i18n";
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    legacy: false,
+    locale: navigator.language.split('-')[0],
+    fallbackLocale: 'en',
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app');
